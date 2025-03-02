aad8830f60467473e8af4ec6cfaf496391ae5d65
sahilmandlik19 <sahilmandlik2022@gmail.com>
2025-03-02 09:45:52 +0530

    Comit Frontend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./Pages/Auth/Login";
-import SignupPage from "./Pages/Auth/Register";
-import HomePage from "./Pages/Home/Home";
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import { useState } from 'react';
+import RefrshHandler from './RefrshHandler';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />
+  }
+
   return (
-    <Router>
+    <div className="App">
+      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/" element={<HomePage />} />
+        <Route path='/' element={<Navigate to="/login" />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
